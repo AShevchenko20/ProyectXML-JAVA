2bbbaa773788a17cb76de0bc4869934f41512d8b
Andrey <ashevche20@jaumebalmes.net>
2021-04-27 22:48:02 +0200

    Andrey Shevchenko modificado Main java

--- Supermercado/src/supermercado/Main.java
+++ Supermercado/src/supermercado/Main.java
@@ -3,6 +3,38 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
 Scanner entrada = new Scanner(System.in);
-    System.out.println("Andrey es un pesao");
-}    
+do{
+System.out.println("Escoje una opcion: ");
+System.out.println("1-Stock");
+System.out.println("2-Vendes");
+System.out.println("3-Sortir");
+int num = entrada.nextInt();
+}
+while(num<1 || num>3);
+if(num==1){Stock();}
+if(num==2){Vendes();}
+if(num==3){Sortir();}
+}
+public static void Stock(){
+System.out.println("Stock");
+System.out.println("1-Afegir");
+System.out.println("2-Vendes");
+System.out.println("3-Sortir");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
